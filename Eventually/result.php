46be93fd4cab8cfea9eb96038ba77d3aabf13f58
aodtns <43024854+symaeng98@users.noreply.github.com>
2021-10-01 19:45:44 +0900

    웹 페이지

---
+++ Eventually/result.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $major = $_POST["major"];
+      $grade = $_POST["grade"];
+      $hour = $_POST["hour"];
+     ?>
+    <?php
+      echo $major;
+     ?>
+  </body>
+</html>
